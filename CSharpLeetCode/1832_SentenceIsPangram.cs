6371b87949a24052d8f4a2239dac4140307360f0
kellhusCorp <alkan_7@mail.ru>
2023-03-20 22:30:41 +0400

    Added solution of https://leetcode.com/problems/check-if-the-sentence-is-pangram/

---
+++ CSharpLeetCode/1832_SentenceIsPangram.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharpLeetCode;
+
+public class SentenceIsPangramSolution
+{
+    public bool CheckIfPangram(string sentence)
+    {
+        var d = new Dictionary<char, int>();
+        foreach (var c in sentence)
+        {
+            if (!d.TryAdd(c, 1))
+            {
+                d[c] += 1;
+            }
+        }
+
+        return d.Keys.Count == 26;
+    }
+}
