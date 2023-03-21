900f2b2fa17f35fb787b0bad16adbcbaadd3844f
kellhusCorp <alkan_7@mail.ru>
2023-03-21 22:04:38 +0400

    Added solution of https://leetcode.com/problems/count-number-of-pairs-with-absolute-difference-k/

---
+++ CSharpLeetCode.Tests/UnitTest2006_CountNumberOfPairs.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharpLeetCode.Tests;
+
+[TestFixture]
+public class UnitTest2006_CountNumberOfPairs
+{
+    [TestCase(new int[] {1,2,2,1}, 1, 4)]
+    [TestCase(new int[] {1,3}, 3, 0)]
+    [TestCase(new int[] {3,2,1,5,4}, 2, 3)]
+    public void Test_Solution_CountKDifference_Returns_4_When_There_Is_4_Pairs(int[] nums, int k, int expected)
+    {
+        var solution = new CountNumberOfPairsSolution();
+
+        var pairs = solution.CountKDifference(nums, k);
+        
+        Assert.That(pairs, Is.EqualTo(expected));
+    }
+}
---
+++ CSharpLeetCode/2006_CountNumberOfPairs.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharpLeetCode;
+
+public class CountNumberOfPairsSolution 
+{
+    public int CountKDifference(int[] nums, int k)
+    {
+        short counter = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i; j < nums.Length; j++)
+            {
+                if (Math.Abs(nums[i] - nums[j]) == k)
+                {
+                    counter++;
+                }
+            }
+        }
+
+        return counter;
+    }
+}
