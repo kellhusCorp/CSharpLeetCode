1c0f9ae42279890a9a3cd01d8a951d7ae0883f88
kellhusCorp <alkan_7@mail.ru>
2023-03-21 22:37:18 +0400

    Added solution of https://leetcode.com/problems/unique-morse-code-words/

---
+++ CSharpLeetCode.Tests/UnitTest804_UniqueMorseCodeWords.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharpLeetCode.Tests;
+
+[TestFixture]
+public class UnitTest804_UniqueMorseCodeWords
+{
+    [TestCase(new[]{"gin","zen","gig","msg"}, 2)]
+    [TestCase(new[]{"a"}, 1)]
+    public void Test_Solution_UniqueMorseRepresentations_Returns_2_When_Exists_2_Different_Transformations_In_Words(string[] words, int expected)
+    {
+        var solution = new UniqueMorseCodeWordsSolution();
+
+        var uniqueMorseRepresentations = solution.UniqueMorseRepresentations(words);
+        
+        Assert.That(uniqueMorseRepresentations, Is.EqualTo(expected));
+    }
+}
---
+++ CSharpLeetCode/804_UniqueMorseCodeWords.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace CSharpLeetCode;
+
+// 1 <= words.length <= 100
+// 1 <= words[i].length <= 12
+
+public class UniqueMorseCodeWordsSolution
+{
+    public int UniqueMorseRepresentations(string[] words)
+    {
+        var fullTable = new[]
+        {
+            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
+        };
+
+        var set = new HashSet<string>();
+
+        foreach (var word in words)
+        {
+            var sb = new StringBuilder();
+            foreach (var c in word) sb.Append(fullTable[c - 97]);
+
+            var finalStr = sb.ToString();
+
+            if (!set.Contains(finalStr)) set.Add(finalStr);
+        }
+
+        return set.Count;
+    }
+}
