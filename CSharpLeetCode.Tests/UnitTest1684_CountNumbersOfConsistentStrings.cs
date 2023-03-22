45adae7215633274069ef75b299bb45d5e6ed312
kellhusCorp <alkan_7@mail.ru>
2023-03-22 22:53:10 +0400

    Added solution of https://leetcode.com/problems/count-the-number-of-consistent-strings/description/

---
+++ CSharpLeetCode.Tests/UnitTest1684_CountNumbersOfConsistentStrings.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace CSharpLeetCode.Tests;
+
+[TestFixture]
+public class UnitTest1684_CountNumbersOfConsistentStrings
+{
+    [TestCase("ab", new [] {"ad","bd","aaab","baa","badab"}, 2)]
+    [TestCase("abc", new [] {"a","b","c","ab","ac","bc","abc"}, 7)]
+    [TestCase("cad", new [] {"cc","acd","b","ba","bac","bad","ac","d"}, 4)]
+    public void Test_Solution_CountConsistentStrings_Returns_2(string allowed, string[] words, int expected)
+    {
+        var sol = new CountNumberOfConsistentStringSolution();
+
+        var result = sol.CountConsistentStrings(allowed, words);
+        
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
---
+++ CSharpLeetCode/1684_CountNumberOfConsistentString.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharpLeetCode;
+
+public class CountNumberOfConsistentStringSolution 
+{
+    public int CountConsistentStrings(string allowed, string[] words)
+    {
+        short counter = 0;
+
+        foreach (var word in words)
+        {
+            var foundDeniedLetter = word.Any(c => !allowed.Contains(c));
+
+            if (!foundDeniedLetter)
+            {
+                counter++;
+            }
+        }
+
+        return counter;
+    } 
+}
