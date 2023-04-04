8658796f77a912e8b6faa23ea546639815550ab1
kellhusCorp <alkan_7@mail.ru>
2023-04-04 08:25:11 +0400

    Added solution of https://leetcode.com/problems/dot-product-of-two-sparse-vectors/

---
+++ CSharpLeetCode.Tests/UnitTest1570_SparseVector.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace CSharpLeetCode.Tests;
+
+[TestFixture]
+public class UnitTest1570SparseVector
+{
+    [Test]
+    [TestCase(new [] {1,0,0,2,3}, new[] {0,3,0,4,0}, 8)]
+    [TestCase(new [] {0,1,0,0,0}, new[] {0,0,0,0,2}, 0)]
+    [TestCase(new [] {0,1,0,0,2,0,0}, new[] {1,0,0,0,3,0,4}, 6)]
+    public void Test(int[] firstNums, int[] secondNums, int expectedResult)
+    {
+        var firstVector = new SparseVector(firstNums);
+        var secondVector = new SparseVector(secondNums);
+
+        var result = firstVector.DotProduct(secondVector);
+        
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
---
+++ CSharpLeetCode/1570_DotProductOfTwoSparseVectors.cs
@@ -0,0 +1,33 @@
+﻿namespace CSharpLeetCode;
+
+public class SparseVector
+{
+    public Dictionary<int, int> Mapping;
+
+    public SparseVector(int[] nums)
+    {
+        Mapping = new Dictionary<int, int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                Mapping[i] = nums[i];
+            }
+        }
+    }
+
+    // Return the dotProduct of two sparse vectors
+    public int DotProduct(SparseVector vec)
+    {
+        var result = 0;
+        foreach (var key in Mapping.Keys)
+        {
+            if (vec.Mapping.ContainsKey(key))
+            {
+                result += Mapping[key] * vec.Mapping[key];
+            }
+        }
+
+        return result;
+    }
+}
