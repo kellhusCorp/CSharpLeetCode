a2b1c86902b7b8e0e7d7a83b9003439900a363e3
kellhusCorp <alkan_7@mail.ru>
2023-05-24 22:16:01 +0400

    Added the solution of https://leetcode.com/problems/merge-nodes-in-between-zeros/description/

---
+++ CSharpLeetCode/2181_MergeNodesInBetweenZeros.cs
@@ -0,0 +1,49 @@
+﻿namespace CSharpLeetCode;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * public int val;
+ * public ListNode next;
+ * public ListNode(int val=0, ListNode next=null) {
+ * this.val = val;
+ * this.next = next;
+ * }
+ * }
+ */
+public class MergeNodesInBetweenZerosSolution
+{
+    public ListNode MergeNodes(ListNode head)
+    {
+        var node = head;
+
+        while (node.next != null)
+        {
+            var next = node.next;
+
+            var temp = next.next;
+            next.next = null;
+
+            if (next.val != 0)
+                node.val += next.val;
+            else if (next.val == 0 && temp != null)
+                node = next;
+
+            node.next = temp;
+        }
+
+        return head;
+    }
+}
