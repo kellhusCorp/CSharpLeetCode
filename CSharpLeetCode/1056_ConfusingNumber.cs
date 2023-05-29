cd70711ec9738240bf1ac91defd9e5ca668d2dc0
Roman <69262893+kellhusCorp@users.noreply.github.com>
2023-05-30 00:20:36 +0400

    Merge pull request #5
    * Added the solution of https://leetcode.com/studyplan/premium-algo-100/

---
+++ CSharpLeetCode/1056_ConfusingNumber.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharpLeetCode;
+
+public class ConfusingNumberSolution
+{
+    private static IDictionary<int, int> _mapping = new Dictionary<int, int>
+    {
+        [0] = 0,
+        [1] = 1,
+        [6] = 9,
+        [8] = 8,
+        [9] = 6
+    };
+
+    public bool ConfusingNumber(int n)
+    {
+        var copy = n;
+        var rotated = 0;
+        while (copy > 0)
+        {
+            var digit = copy % 10;
+            if (!_mapping.ContainsKey(digit))
+            {
+                return false;
+            }
+
+            rotated = rotated * 10 + _mapping[digit];
+            copy /= 10;
+        }
+
+        return rotated != n;
+    }
+}
