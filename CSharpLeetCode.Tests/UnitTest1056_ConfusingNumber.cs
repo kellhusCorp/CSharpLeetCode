55def9d1574c58c872f70b638b4ec6afb3ee1ea4
kellhusCorp <alkan_7@mail.ru>
2023-05-30 00:20:01 +0400

    Added the solution of https://leetcode.com/studyplan/premium-algo-100/

---
+++ CSharpLeetCode.Tests/UnitTest1056_ConfusingNumber.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace CSharpLeetCode.Tests;
+
+[TestFixture]
+public class UnitTest1056ConfusingNumber
+{
+    [Test]
+    [TestCase(6, true)]
+    [TestCase(89, true)]
+    [TestCase(11, false)]
+    [TestCase(8000, true)]
+    public void Test_ConfusingNumber_Returns_ResultExpected_When_N_Equals_N(int n, bool expected)
+    {
+        var sol = new ConfusingNumberSolution();
+
+        var result = sol.ConfusingNumber(n);
+        
+        Assert.That(result, expected ? Is.True : Is.False);
+    }
+}
