e831a7411f2a290c57d2a44a81cea598917fd52a
kellhusCorp <kellhusCorp@gmail.com>
2023-07-16 02:18:13 +0400

    Added the solution of https://leetcode.com/problems/flipping-an-image/description/

---
+++ CSharpLeetCode.Tests/UnitTest832_FlippingImage.cs
@@ -0,0 +1,54 @@
+﻿namespace CSharpLeetCode.Tests;
+
+[TestFixture]
+public class UnitTest832_FlippingImage
+{
+    [Test]
+    public void Test_FlippingImage_FlipAndInvertImage()
+    {
+        var image = new[]
+        {
+            new[] { 1, 1, 0 }, new[] { 1, 0, 1 }, new[] { 0, 0, 0 }
+        };
+
+        var result = new FlippingImageSolution().FlipAndInvertImage(image);
+
+        Assert.That(result, Is.EqualTo(new int[3][]
+        {
+            new[] { 1, 0, 0 },
+            new[] { 0, 1, 0 },
+            new[] { 1, 1, 1 }
+        }));
+    }
+
+    [Test]
+    [TestCase(new[] { 1, 1, 0 }, new[] { 0, 1, 1 })]
+    [TestCase(new[] { 1, 1, 0, 1 }, new[] { 1, 0, 1, 1 })]
+    public void Test_FlippingImage_HorizontallyFlipRow(int[] row, int[] expected)
+    {
+        var result = new FlippingImageSolution().HorizontallyFlipRow(row);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    [TestCase(new[] { 1, 1, 0 }, new[] { 0, 0, 1 })]
+    [TestCase(new[] { 0, 0, 0, 0 }, new[] { 1, 1, 1, 1 })]
+    public void Test_FlippingImage_InvertRow(int[] row, int[] expected)
+    {
+        var result = new FlippingImageSolution().InvertRow(row);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    [TestCase(new[] { 1, 1, 0 }, new[] { 1, 0, 0 })]
+    [TestCase(new[] { 1, 0, 1 }, new[] { 0, 1, 0 })]
+    [TestCase(new[] { 0, 0, 0 }, new[] { 1, 1, 1 })]
+    public void Test_FlippingImage_FlipAndInvertRow(int[] row, int[] expected)
+    {
+        var result = new FlippingImageSolution().FlipAndInvertRow(row);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
---
+++ CSharpLeetCode/832_FlippingImage.cs
@@ -0,0 +1,47 @@
+﻿namespace CSharpLeetCode;
+
+public class FlippingImageSolution
+{
+    private readonly Func<int[], int[]> _flipAction;
+
+    public FlippingImageSolution()
+    {
+        _flipAction += HorizontallyFlipRow;
+        _flipAction += InvertRow;
+    }
+
+    public int[][] FlipAndInvertImage(int[][] image)
+    {
+        for (var i = 0; i < image.Length; i++)
+        {
+            image[i] = FlipAndInvertRow(image[i]);
+        }
+
+        return image;
+    }
+
+    public int[] FlipAndInvertRow(int[] row)
+    {
+        return _flipAction(row);
+    }
+
+    public int[] HorizontallyFlipRow(int[] row)
+    {
+        for (int i = 0, j = row.Length - 1; j > i; i++, j--)
+        {
+            (row[i], row[j]) = (row[j], row[i]);
+        }
+
+        return row;
+    }
+
+    public int[] InvertRow(int[] row)
+    {
+        for (var i = 0; i < row.Length; i++)
+        {
+            row[i] = row[i] == 0 ? 1 : 0;
+        }
+
+        return row;
+    }
+}
