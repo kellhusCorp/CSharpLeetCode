47c8a4e55210dd317e1c26765b754cd533876cc0
Roman <69262893+kellhusCorp@users.noreply.github.com>
2023-07-17 23:44:38 +0400

    Merge pull request #11
    * Added solution of https://leetcode.com/problems/merge-sorted-array/de…

---
+++ CSharpLeetCode/88_MergeSortedArray.cs
@@ -0,0 +1,26 @@
+﻿namespace CSharpLeetCode;
+
+public class MergeSortedArraySolution 
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        var l = m - 1;
+        var r = n - 1;
+
+        for (int i = m + n - 1; i >= 0; i--)
+        {
+            if (r < 0)
+            {
+                break;
+            }
+            if (l >= 0 && nums1[l] > nums2[r])
+            {
+                nums1[i] = nums1[l--];
+            }
+            else
+            {
+                nums1[i] = nums2[r--];
+            }
+        }
+    }
+}
