9edc92843e0fd3fece0f3ae82841282c0d8b54ca
kellhusCorp <kellhusCorp@gmail.com>
2023-09-16 00:15:39 +0400

    Added solution of https://leetcode.com/problems/deepest-leaves-sum/description/

---
+++ CSharpLeetCode.Tests/UnitTest1302_DeepestLeavesSum.cs
@@ -0,0 +1,52 @@
+﻿using CSharpLeetCode.Deepest;
+
+namespace CSharpLeetCode.Tests
+{
+    public static class TreeNodeFactory
+    {
+        public enum Variant
+        {
+            First,
+            Second,
+            Third,
+            Fourth
+        }
+
+        public static Deepest.TreeNode Create(Variant variant)
+        {
+            switch (variant)
+            {
+                case Variant.First:
+                    return new Deepest.TreeNode(1, new Deepest.TreeNode(2, new Deepest.TreeNode(4, new Deepest.TreeNode(7)), new Deepest.TreeNode(5)),
+                        new Deepest.TreeNode(3, null, new Deepest.TreeNode(6, null, new Deepest.TreeNode(8))));
+                case Variant.Second:
+                    return new Deepest.TreeNode(6, new Deepest.TreeNode(7, new Deepest.TreeNode(2, new Deepest.TreeNode(9)), new Deepest.TreeNode(7, new Deepest.TreeNode(1), new Deepest.TreeNode(4))),
+                        new Deepest.TreeNode(8, new Deepest.TreeNode(1), new Deepest.TreeNode(3, null, new Deepest.TreeNode(5))));
+                case Variant.Third:
+                    return new Deepest.TreeNode(1);
+                case Variant.Fourth:
+                    return new Deepest.TreeNode(0, null, new Deepest.TreeNode(1));
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(variant), variant, null);
+            }
+        }
+    }
+    
+    [TestFixture]
+    public class UnitTest1302DeepestLeavesSum
+    {
+        [Test]
+        [TestCase(TreeNodeFactory.Variant.First, 15)]
+        [TestCase(TreeNodeFactory.Variant.Second, 19)]
+        [TestCase(TreeNodeFactory.Variant.Third, 1)]
+        [TestCase(TreeNodeFactory.Variant.Fourth, 1)]
+        public void Test_DeepestLeavesSumSolution_DeepestLeavesSum_When_Root_Equals_Variant(TreeNodeFactory.Variant variant, int expected)
+        {
+            var root = TreeNodeFactory.Create(variant);
+
+            var result = new DeepestLeavesSumSolution().DeepestLeavesSum(root);
+            
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ CSharpLeetCode/1302_DeepestLeavesSum.cs
@@ -0,0 +1,78 @@
+﻿namespace CSharpLeetCode.Deepest
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public static class TreeNodeExtensions
+    {
+        public static bool IsExistChildren(this TreeNode node)
+        {
+            return node.left != null || node.right != null;
+        }
+    }
+    
+    public class DeepestLeavesSumSolution
+    {
+        public int DeepestLeavesSum(TreeNode root)
+        {
+            int sum = 0, depth = 0, currentDepth;
+            
+            var stack = new Stack<(TreeNode Node, int Depth)>();
+            
+            stack.Push((root, 0));
+
+            while (stack.Any())
+            {
+                var pair = stack.Pop();
+
+                var node = pair.Node;
+
+                currentDepth = pair.Depth;
+                
+                if (!node.IsExistChildren())
+                {
+                    if (depth < currentDepth)
+                    {
+                        sum = node.val;
+                        depth = currentDepth;
+                    }
+                    else if (depth == currentDepth)
+                    {
+                        sum += node.val;
+                    }
+                }
+
+                else
+                {
+                    PushChildren(node, stack, currentDepth);
+                }
+            }
+
+            return sum;
+        }
+
+        private static void PushChildren(TreeNode node, Stack<(TreeNode Node, int Depth)> stack, int currentDepth)
+        {
+            if (node.left != null)
+            {
+                stack.Push((node.left, currentDepth + 1));
+            }
+
+            if (node.right != null)
+            {
+                stack.Push((node.right, currentDepth + 1));
+            }
+        }
+    }
+}
