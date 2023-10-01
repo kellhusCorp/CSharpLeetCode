8fe6c119801d61b9bdfe231fd101478b6009b321
Roman <69262893+kellhusCorp@users.noreply.github.com>
2023-10-01 23:36:43 +0400

    Merge pull request #14
    * Added solution of https://leetcode.com/problems/design-sql/submissions/

---
+++ CSharpLeetCode/2408_DesignSQL.cs
@@ -0,0 +1,54 @@
+﻿namespace CSharpLeetCode
+{
+    public class Table
+    {
+        private int _id;
+        private readonly Dictionary<int, string[]> _rows = new();
+
+        public void InsertRow(string[] values)
+        {
+            _rows.Add(++_id, values);
+        }
+
+        public void DeleteRow(int rowId)
+        {
+            _rows.Remove(rowId);
+        }
+        
+        public string SelectCell(int rowId, int columnId)
+        {
+            return _rows[rowId][columnId - 1];
+        }
+    }
+
+    public class SQL
+    {
+        private readonly IDictionary<string, Table> _tables = new Dictionary<string, Table>();
+        
+        public SQL(IList<string> names, IList<int> columns) 
+        {
+            foreach (string name in names)
+            {
+                if (!_tables.ContainsKey(name))
+                {
+                    _tables.Add(name, new Table());
+                }
+            }
+        }
+    
+        public void InsertRow(string name, IList<string> row) 
+        {
+            _tables[name].InsertRow(row.ToArray());
+        }
+    
+        public void DeleteRow(string name, int rowId) 
+        {
+            _tables[name].DeleteRow(rowId);
+        }
+    
+        public string SelectCell(string name, int rowId, int columnId)
+        {
+            return _tables[name].SelectCell(rowId, columnId);
+        }
+    }
+}
