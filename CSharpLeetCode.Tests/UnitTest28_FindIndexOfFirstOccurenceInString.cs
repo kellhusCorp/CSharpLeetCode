68edd5260a98917e0be5d15c35e52f2da19855b8
Roman <69262893+kellhusCorp@users.noreply.github.com>
2023-10-16 15:42:57 +0400

    Merge pull request #17
    * Added solution of https://leetcode.com/problems/find-the-index-of-the…

---
+++ CSharpLeetCode.Tests/UnitTest28_FindIndexOfFirstOccurenceInString.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpLeetCode.Tests
+{
+    [TestFixture]
+    public class UnitTest28_FindIndexOfFirstOccurenceInString
+    {
+        [Test]
+        [TestCase("sadbutsad", "sad", 0)]
+        [TestCase("leetcode", "leeto", -1)]
+        [TestCase("youneedtobestrong", "tobe", 7)]
+        [TestCase("ttpppttttzzzzzreprp", "tttt", 5)]
+        [TestCase("notfound", "notfound", 0)]
+        public void Test_FindIndexOfFirstOccurenceInStringSolution_StrStr_Returns_Expected(string haystack,
+            string needle, int expected)
+        {
+            var result = new FindIndexOfFirstOccurenceInStringSolution().StrStr(haystack, needle);
+            
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ CSharpLeetCode/28_FindIndexOfFirstOccurenceInString.cs
@@ -0,0 +1,31 @@
+﻿namespace CSharpLeetCode
+{
+    public class FindIndexOfFirstOccurenceInStringSolution 
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            var n = haystack.Length;
+            var m = needle.Length;
+
+            for (int i = 0; i <= n - m; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (needle[j] != haystack[i + j])
+                    {
+                        break;
+                    }
+
+                    if (j == m - 1)
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            return -1;
+
+            // return haystack.IndexOf(needle, StringComparison.Ordinal);
+        }
+    }
+}
