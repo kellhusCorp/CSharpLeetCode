ed11a2473858223befc297b6b1b015de14f5d7df
kellhusCorp <kellhusCorp@gmail.com>
2023-10-22 23:33:56 +0400

    Added solution of https://leetcode.com/problems/length-of-last-word/description/

---
+++ CSharpLeetCode.Tests/UnitTest58_LengthOfLastWord.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharpLeetCode.Tests
+{
+    public class UnitTest58_LengthOfLastWord
+    {
+        [Test]
+        [TestCase("Hello World", 5)]
+        [TestCase(" ", 0)]
+        [TestCase("a ", 1)]
+        [TestCase("a", 1)]
+        [TestCase("a b", 1)]
+        [TestCase("   fly me   to   the moon  ", 4)]
+        public void Test_LengthOfLastWordSolution_LengthOfLastWord_ReturnsCorrectLength(string s, int expected)
+        {
+            var actual = new LengthOfLastWordSolution().LengthOfLastWord(s);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
