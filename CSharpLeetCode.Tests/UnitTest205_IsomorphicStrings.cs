04cc89bf74011192482ca2a48624df4629e81bfe
kellhusCorp <kellhusCorp@gmail.com>
2023-10-23 00:13:36 +0400

    Added solution of https://leetcode.com/problems/isomorphic-strings/description

---
+++ CSharpLeetCode.Tests/UnitTest205_IsomorphicStrings.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharpLeetCode.Tests
+{
+    public class UnitTest205_IsomorphicStrings
+    {
+        [Test]
+        [TestCase("egg", "add", true)]
+        [TestCase("foo", "bar", false)]
+        [TestCase("paper", "title", true)]
+        [TestCase("bbbaaaba", "aaabbbba", false)]
+        public void Test_IsomorphicStringsSolution_IsIsomorphic_Returns_Expected(string s, string t, bool expected)
+        {
+            var sol = new IsomorphicStringsSolution();
+
+            var result = sol.IsIsomorphic(s, t);
+            
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ CSharpLeetCode/205_IsomorphicStrings.cs
@@ -0,0 +1,42 @@
+﻿namespace CSharpLeetCode
+{
+    public class IsomorphicStringsSolution 
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            var sTransformed = Transform(s);
+            var tTransformed = Transform(t);
+            return Equals(sTransformed, tTransformed);
+        }
+
+        private static bool Equals(IReadOnlyList<int> firstArr, IReadOnlyList<int> secondArr)
+        {
+            if (firstArr.Count != secondArr.Count)
+                return false;
+
+            for (int i = 0; i < firstArr.Count; i++)
+            {
+                if (firstArr[i] != secondArr[i]) return false;
+            }
+
+            return true;
+        }
+        
+        private static int[] Transform(string s)
+        {
+            var d = new Dictionary<char, int>();
+            var arr = new int[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!d.ContainsKey(s[i]))
+                {
+                    d[s[i]] = i;
+                }
+
+                arr[i] = d[s[i]];
+            }
+
+            return arr;
+        }
+    }
+}
