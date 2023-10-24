bebb00820bee32faf76b6262f86e3a71576da70d
Roman <69262893+kellhusCorp@users.noreply.github.com>
2023-10-24 16:59:26 +0400

    Added solution of https://leetcode.com/problems/length-of-last-word/description/ (#18)
    Co-authored-by: kellhusCorp <kellhusCorp@gmail.com>

---
+++ CSharpLeetCode/58_LengthOfLastWord.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharpLeetCode
+{
+    public class LengthOfLastWordSolution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            var length = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == ' ' && length > 0)
+                {
+                    break;
+                }
+
+                if (s[i] != ' ')
+                {
+                    length++;
+                }
+            }
+
+            return length;
+        }
+    }
+}
