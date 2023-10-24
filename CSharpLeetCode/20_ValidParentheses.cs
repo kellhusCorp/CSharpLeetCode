d76fa00c90810b8c9fab24feeb67d387caacff13
Roman <69262893+kellhusCorp@users.noreply.github.com>
2023-10-24 17:01:04 +0400

    Added solution of https://leetcode.com/problems/valid-parentheses/description (#20)
    Co-authored-by: kellhusCorp <kellhusCorp@gmail.com>

---
+++ CSharpLeetCode/20_ValidParentheses.cs
@@ -0,0 +1,40 @@
+﻿namespace CSharpLeetCode
+{
+    public class ValidParenthesesSolution
+    {
+        private static readonly IReadOnlyCollection<char> BracesOpened = new List<char> { '(', '{', '[' };
+
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var c in s)
+            {
+                if (IsOpened(c))
+                {
+                    stack.Push(GetReverse(c));
+                }
+                else
+                {
+                    stack.TryPop(out char brace);
+                    if (brace != c) return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+
+        private static bool IsOpened(char c) => BracesOpened.Contains(c);
+
+        private static char GetReverse(char c)
+        {
+            return c switch
+            {
+                '(' => ')',
+                '{' => '}',
+                '[' => ']',
+                _ => throw new ArgumentOutOfRangeException(nameof(c))
+            };
+        }
+    }
+}
