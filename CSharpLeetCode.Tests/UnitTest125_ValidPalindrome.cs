5dcac33bd0a5b5f5d946edf5b77d64f2fb11c7ec
Roman <69262893+kellhusCorp@users.noreply.github.com>
2023-11-14 19:29:09 +0400

    Merge pull request #23
    * Added solution of https://leetcode.com/problems/valid-palindrome/desc…

---
+++ CSharpLeetCode.Tests/UnitTest125_ValidPalindrome.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharpLeetCode.Tests
+{
+    [TestFixture]
+    public class UnitTest125_ValidPalindrome
+    {
+        [Test]
+        [TestCase("A man, a plan, a canal: Panama", true)]
+        [TestCase("race a car", false)]
+        [TestCase(" ", true)]
+        public void Test_ValidPalindrome_IsValid(string s, bool expected)
+        {
+            var result = new ValidPalindromeSolution().IsPalindrome(s);
+            
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
