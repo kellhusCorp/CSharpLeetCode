3b0f595bb9efdb0f179ec04dec0896077934ee6b
Roman <69262893+kellhusCorp@users.noreply.github.com>
2023-11-14 18:41:57 +0400

    Merge pull request #22
    * Added solution of https://leetcode.com/problems/longest-common-prefix…

---
+++ CSharpLeetCode/14_LongestCommonPrefix.cs
@@ -0,0 +1,29 @@
+﻿namespace CSharpLeetCode
+{
+    public class LongestCommonPrefixSolution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (!strs.Any())
+            {
+                return string.Empty;
+            }
+
+            var prefix = strs[0];
+
+            for (int i = 1; i < strs.Length; i++)
+            {
+                while (strs[i].IndexOf(prefix) != 0)
+                {
+                    prefix = prefix.Substring(0, prefix.Length - 1);
+                    if (!prefix.Any())
+                    {
+                        return string.Empty;
+                    }
+                }
+            }
+
+            return prefix;
+        }
+    }
+}
