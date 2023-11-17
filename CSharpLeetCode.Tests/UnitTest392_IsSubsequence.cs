50b634a178202b5f2d334270ac2962c3dbf3cf04
Roman <69262893+kellhusCorp@users.noreply.github.com>
2023-11-17 20:22:59 +0400

    Merge pull request #24
    * Added solution of https://leetcode.com/problems/is-subsequence/descri…

---
+++ CSharpLeetCode.Tests/UnitTest392_IsSubsequence.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharpLeetCode.Tests
+{
+    [TestFixture]
+    public class UnitTest392_IsSubsequence
+    {
+        [Test]
+        [TestCase("abc", "ahbgdc", true)]
+        [TestCase("axc", "ahbgdc", false)]
+        [TestCase("acb", "ahbgdc", false)]
+        public void Test_IsSubsequenceSolution_IsSubsequence(string s, string t, bool expected)
+        {
+            var sol = new IsSubsequenceSolution();
+
+            var result = sol.IsSubsequence(s, t);
+            
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
