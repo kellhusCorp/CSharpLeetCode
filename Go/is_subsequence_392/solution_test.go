15d20bdc4218c0f424a17fe5efd780959122f441
kellhusCorp <kellhusCorp@gmail.com>
2023-11-17 20:41:19 +0400

    Added go solution's folder

---
+++ Go/is_subsequence_392/solution_test.go
@@ -0,0 +1,14 @@
+package is_subsequence_392
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_IsSubsequence392(t *testing.T) {
+	s := "abc"
+	testStr := "ahbgdc"
+	expected := true
+	actual := isSubsequence(s, testStr)
+	assert.Equal(t, expected, actual)
+}
