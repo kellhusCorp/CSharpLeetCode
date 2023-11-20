3a07ef9d4eb8eaa8883d4222d8cf2d1eb37331d4
Roman <69262893+kellhusCorp@users.noreply.github.com>
2023-11-20 10:30:16 +0400

    Merge pull request #27
    * Added solution of https://leetcode.com/problems/word-pattern/

---
+++ CSharpLeetCode.Tests/UnitTest290_WordPattern.cs
@@ -0,0 +1,36 @@
+﻿namespace CSharpLeetCode.Tests
+{
+    [TestFixture]
+    public class UnitTest290_WordPattern
+    {
+        [Test]
+        [TestCase("abba", "dog cat cat dog", true)]
+        [TestCase("abba", "dog cat cat fish", false)]
+        [TestCase("aaaa", "dog cat cat dog", false)]
+        [TestCase("abba", "dog dog dog dog", false)]
+        [TestCase("aaa", "aa aa aa aa", false)]
+        public void Test_WordPatternSolution_WordPattern(string pattern, string s, bool expected)
+        {
+            var sol = new WordPatternSolution();
+
+            var result = sol.WordPattern(pattern, s);
+            
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        
+        [Test]
+        [TestCase("abba", "dog cat cat dog", true)]
+        [TestCase("abba", "dog cat cat fish", false)]
+        [TestCase("aaaa", "dog cat cat dog", false)]
+        [TestCase("abba", "dog dog dog dog", false)]
+        [TestCase("aaa", "aa aa aa aa", false)]
+        public void Test_WordPatternSolution_WordPatternAnotherOne(string pattern, string s, bool expected)
+        {
+            var sol = new WordPatternSolution();
+
+            var result = sol.WordPattern(pattern, s);
+            
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
