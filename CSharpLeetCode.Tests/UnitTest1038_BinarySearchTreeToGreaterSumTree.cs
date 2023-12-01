5c13530cfda46a127ce97f7bf652f3591b4bd814
kellhusCorp <kellhusCorp@gmail.com>
2023-12-01 20:50:52 +0400

    Added the solution of https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/description/

---
+++ CSharpLeetCode.Tests/UnitTest1038_BinarySearchTreeToGreaterSumTree.cs
@@ -0,0 +1,47 @@
+﻿namespace CSharpLeetCode.Tests
+{
+    [TestFixture]
+    public class UnitTest1038_BinarySearchTreeToGreaterSumTree
+    {
+        private TreeNode _firstTree;
+        private TreeNode _secondTree;
+        
+        [SetUp]
+        public void Init()
+        {
+            _firstTree = new TreeNode(4, new TreeNode(1, new TreeNode(0), new TreeNode(2, null, new TreeNode(3))),
+                new TreeNode(6, new TreeNode(5), new TreeNode(7, null, new TreeNode(8))));
+            _secondTree = new TreeNode(0, null, new TreeNode(1));
+        }
+        
+        [Test]
+        public void Test_BinarySearchTreeToGreaterSumTree_FirstCase()
+        {
+            BinarySearchTreeToGreaterSumTreeSolution solution = new BinarySearchTreeToGreaterSumTreeSolution();
+
+            TreeNode result = solution.BstToGst(_firstTree);
+
+            Assert.That(result.val, Is.EqualTo(30));
+        }
+        
+        [Test]
+        public void Test_BinarySearchTreeToGreaterSumTree_SecondCase()
+        {
+            var solution = new BinarySearchTreeToGreaterSumTreeSolution();
+            
+            var result = solution.BstToGst(_secondTree);
+            
+            Assert.That(result.val, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Test_BinarySearchTreeToGreaterSumTree_Recursively()
+        {
+            var solution = new BinarySearchTreeToGreaterSumTreeSolution();
+            
+            var result = solution.BstToGstRecursively(_firstTree);
+            
+            Assert.That(result.val, Is.EqualTo(30));
+        }
+    }
+}
---
+++ CSharpLeetCode/1038_BinarySearchTreeToGreaterSumTree.cs
@@ -0,0 +1,44 @@
+﻿namespace CSharpLeetCode
+{
+    public class BinarySearchTreeToGreaterSumTreeSolution
+    {
+        private int _sum = 0;
+        
+        public TreeNode BstToGst(TreeNode root)
+        {
+            var sum = 0;
+            var currentNode = root;
+            var stack = new Stack<TreeNode>();
+            while (stack.Any() || currentNode != null)
+            {
+                while (currentNode != null)
+                {
+                    stack.Push(currentNode);
+                    currentNode = currentNode.right;
+                }
+
+                currentNode = stack.Pop();
+                sum += currentNode.val;
+                currentNode.val = sum;
+
+                currentNode = currentNode.left;
+            }
+
+            return root;
+        }
+        
+        public TreeNode BstToGstRecursively(TreeNode root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+            
+            BstToGstRecursively(root.right);
+            _sum += root.val;
+            root.val = _sum;
+            BstToGstRecursively(root.left);
+            return root;
+        }
+    }
+}
