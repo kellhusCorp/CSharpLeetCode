ad19dc85079b3534aa740e41901edf03b24f2005
Roman <69262893+kellhusCorp@users.noreply.github.com>
2023-12-14 16:32:09 +0400

    Merge pull request #33
    * Added solution of https://leetcode.com/problems/same-tree/

---
+++ CSharpLeetCode.Tests/UnitTest100_SameTree.cs
@@ -0,0 +1,38 @@
+﻿namespace CSharpLeetCode.Tests
+{
+    [TestFixture]
+    public class UnitTest100_SameTree
+    {
+        [Test]
+        public void Test_SameTreeSolution_IsSameTree_FirstCase()
+        {
+            var p = new TreeNode(1);
+            p.left = new TreeNode(2);
+            p.right = new TreeNode(3);
+            var q = new TreeNode(1);
+            q.left = new TreeNode(2);
+            q.right = new TreeNode(3);
+            var expected = true;
+            var solution = new SameTreeSolution();
+            
+            var actual = solution.IsSameTree(p, q);
+            
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void Test_SameTreeSolution_IsSameTree_SecondCase()
+        {
+            var p = new TreeNode(1);
+            p.left = new TreeNode(2);
+            var q = new TreeNode(1);
+            q.right = new TreeNode(2);
+            var expected = false;
+            var solution = new SameTreeSolution();
+            
+            var actual = solution.IsSameTree(p, q);
+            
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ CSharpLeetCode/100_SameTree.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharpLeetCode
+{
+    public class SameTreeSolution
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q) 
+        {
+            if (p == null && q == null)
+            {
+                return true;
+            }
+
+            if (p == null || q == null)
+            {
+                return false;
+            }
+            
+            if (p.val != q.val)
+            {
+                return false;
+            }
+
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right) && p.val == q.val;
+        }
+    }
+}
