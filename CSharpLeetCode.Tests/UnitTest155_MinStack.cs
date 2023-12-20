8c724f2331eb0c0db6034e49a76b31ac3636f1a2
Roman <69262893+kellhusCorp@users.noreply.github.com>
2023-12-21 00:48:53 +0400

    Merge pull request #34
    * Added the solution to https://leetcode.com/problems/min-stack/descrip…
    
    * Updated README.md

---
+++ CSharpLeetCode.Tests/UnitTest155_MinStack.cs
@@ -0,0 +1,34 @@
+﻿namespace CSharpLeetCode.Tests
+{
+    [TestFixture]
+    public class UnitTest155_MinStack
+    {
+        [Test]
+        public void Test_MinStack_FirstCase()
+        {
+            var minStack = new MinStack();
+            minStack.Push(-2);
+            minStack.Push(0);
+            minStack.Push(-3);
+            Assert.That(minStack.GetMin(), Is.EqualTo(-3));
+            minStack.Pop();
+            Assert.Multiple(() =>
+            {
+                Assert.That(minStack.Top(), Is.EqualTo(0));
+                Assert.That(minStack.GetMin(), Is.EqualTo(-2));
+            });
+        }
+        
+        [Test]
+        public void Test_MinStack_SecondCase()
+        {
+            var minStack = new MinStack();
+            minStack.Push(0);
+            minStack.Push(1);
+            minStack.Push(0);
+            Assert.That(minStack.GetMin(), Is.EqualTo(0));
+            minStack.Pop();
+            Assert.That(minStack.GetMin(), Is.EqualTo(0));
+        }
+    }
+}
---
+++ CSharpLeetCode/155_MinStack.cs
@@ -0,0 +1,41 @@
+﻿namespace CSharpLeetCode
+{
+    public class MinStack
+    {
+        private readonly LinkedList<int> _items = new();
+        private readonly LinkedList<int> _mins = new();
+    
+        public void Push(int val)
+        {
+            if (!_mins.Any() || val <= _mins.Last.Value) 
+            {
+                _mins.AddLast(val);
+            }
+            _items.AddLast(val);
+        }
+    
+        public void Pop()
+        {
+            if (!_items.Any())
+                throw new InvalidOperationException();
+            if (_items.Last.Value == _mins.Last.Value)
+            {
+                _mins.RemoveLast();
+            }
+            
+            _items.RemoveLast();
+        }
+    
+        public int Top()
+        {
+            if (!_items.Any())
+                throw new InvalidOperationException();
+            return _items.Last.Value;
+        }
+    
+        public int GetMin()
+        {
+            return _mins.Any() ? _mins.Last.Value : int.MaxValue;
+        }
+    }
+}
