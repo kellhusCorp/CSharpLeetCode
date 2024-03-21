58947f1a4df102abef0d22b23baffb15ec940c88
kellhusCorp <kellhusCorp@gmail.com>
2024-03-21 09:07:29 +0400

    The solution to https://leetcode.com/problems/evaluate-reverse-polish-notation/ was added

---
+++ CSharpLeetCode/150_EvaluateReversePolishNotation.cs
@@ -0,0 +1,34 @@
+﻿namespace CSharpLeetCode
+{
+    public class EvaluateReversePolishNotationSolution
+    {
+        private readonly IDictionary<string, Func<int, int, int>> _operators = new Dictionary<string, Func<int, int, int>>()
+        {
+            ["+"] = (x, y) => x + y,
+            ["-"] = (x, y) => x - y,
+            ["*"] = (x, y) => x * y,
+            ["/"] = (x, y) => x / y
+        };
+        
+        public int EvalRPN(string[] tokens)
+        {
+            var operations = new Stack<int>();
+
+            foreach (string token in tokens)
+            {
+                if (!_operators.ContainsKey(token))
+                {
+                    operations.Push(int.Parse(token));
+                }
+                else
+                {
+                    var firstElement = operations.Pop();
+                    var secondElement = operations.Pop();
+                    operations.Push(_operators[token](secondElement, firstElement));
+                }
+            }
+
+            return operations.Pop();
+        }
+    }
+}
