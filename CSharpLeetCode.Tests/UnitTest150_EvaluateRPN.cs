757da6db524284d461e2ea63b8aa8cbb1e9ba8f2
Roman <69262893+kellhusCorp@users.noreply.github.com>
2024-03-21 09:09:00 +0400

    Merge pull request #37
    * The solution to https://leetcode.com/problems/evaluate-reverse-polish…
    
    * Updated README.md

---
+++ CSharpLeetCode.Tests/UnitTest150_EvaluateRPN.cs
@@ -0,0 +1,42 @@
+﻿namespace CSharpLeetCode.Tests
+{
+    [TestFixture]
+    public class UnitTest150_EvaluateRPN
+    {
+        [Test]
+        public void Test_EvaluateReversePolishNotationSolution_EvalRPN_FirstCase()
+        {
+            var tokens = new[] { "2", "1", "+", "3", "*" };
+
+            var sol = new EvaluateReversePolishNotationSolution();
+
+            var result = sol.EvalRPN(tokens);
+            
+            Assert.That(result, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void Test_EvaluateReversePolishNotationSolution_EvalRPN_SecondCase()
+        {
+            var tokens = new[] { "4","13","5","/","+" };
+
+            var sol = new EvaluateReversePolishNotationSolution();
+
+            var result = sol.EvalRPN(tokens);
+            
+            Assert.That(result, Is.EqualTo(6));
+        }
+        
+        [Test]
+        public void Test_EvaluateReversePolishNotationSolution_EvalRPN_ThirdCase()
+        {
+            var tokens = new[] { "10","6","9","3","+","-11","*","/","*","17","+","5","+" };
+
+            var sol = new EvaluateReversePolishNotationSolution();
+
+            var result = sol.EvalRPN(tokens);
+            
+            Assert.That(result, Is.EqualTo(22));
+        }
+    }
+}
