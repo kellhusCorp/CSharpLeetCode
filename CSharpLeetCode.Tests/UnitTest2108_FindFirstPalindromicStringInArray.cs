68b88bed43f1eba579607d09af2cba42276bafa4
kellhusCorp <kellhusCorp@gmail.com>
2024-04-11 11:27:33 +0400

    Added the solution to https://leetcode.com/problems/find-first-palindromic-string-in-the-array/description/

---
+++ CSharpLeetCode.Tests/UnitTest2108_FindFirstPalindromicStringInArray.cs
@@ -0,0 +1,50 @@
+﻿namespace CSharpLeetCode.Tests
+{
+    public class UnitTest2108_FindFirstPalindromicStringInArray
+    {
+        [Test]
+        public void Test_FindFirstPalindromicStringInArraySolution_FirstPalindrome_FirstCase()
+        {
+            var words = new string[] {"abc","car","ada","racecar","cool"};
+            var sol = new FindFirstPalindromicStringInArraySolution();
+            
+            var result = sol.FirstPalindrome(words);
+            
+            Assert.That(result, Is.EqualTo(words[2]));
+        }
+        
+        [Test]
+        public void Test_FindFirstPalindromicStringInArraySolution_FirstPalindrome_SecondCase()
+        {
+            var words = new string[] {"notapalindrome", "racecar"};
+            var sol = new FindFirstPalindromicStringInArraySolution();
+
+            var result = sol.FirstPalindrome(words);
+            
+            Assert.That(result, Is.EqualTo(words[1]));
+        }
+        
+        [Test]
+        public void Test_FindFirstPalindromicStringInArraySolution_FirstPalindrome_ThirdCase()
+        {
+            var words = new string[] {"def","ghi"};
+            var sol = new FindFirstPalindromicStringInArraySolution();
+
+            var result = sol.FirstPalindrome(words);
+            
+            Assert.That(result, Is.EqualTo(string.Empty));
+        }
+        
+        [Test]
+        public void Test_FindFirstPalindromicStringInArraySolution_FirstPalindrome_OneHundredEightiethCase()
+        {
+            var words = new string[] {"po","zsz"};
+            var sol = new FindFirstPalindromicStringInArraySolution();
+
+            var result = sol.FirstPalindrome(words);
+            
+            Assert.That(result, Is.EqualTo(words[1]));
+        }
+
+    }
+}
