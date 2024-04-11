dfb38a610fc1a57f89ff3539351671cd74f4926a
Roman <69262893+kellhusCorp@users.noreply.github.com>
2024-04-11 11:28:37 +0400

    Merge pull request #40
    * Added the solution to https://leetcode.com/problems/find-first-palind…
    
    * Updated README.md

---
+++ CSharpLeetCode/2108_FindFirstPalindromicStringInArray.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharpLeetCode
+{
+    public class FindFirstPalindromicStringInArraySolution
+    {
+        public string FirstPalindrome(string[] words)
+        {
+            foreach (var word in words)
+            {
+                if (IsPalindrome(word)) return word;
+            }
+
+            return string.Empty;
+        }
+
+        private static bool IsPalindrome(string word)
+        {
+            for(int i = 0, j = word.Length - 1; i < word.Length / 2; i++, j--)
+            {
+                if (word[i] != word[j]) return false;
+            }
+
+            return true;
+        }
+    }
+}
