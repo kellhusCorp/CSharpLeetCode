eb185afce32f0f07681f563375cec1d779ca23f2
kellhusCorp <kellhusCorp@gmail.com>
2024-04-15 17:33:19 +0400

    Added the linear search solution to https://leetcode.com/problems/missing-number-in-arithmetic-progression/

---
+++ CSharpLeetCode.Tests/UnitTest1228_MissingNumberInArithmeticProgression.cs
@@ -0,0 +1,29 @@
+﻿namespace CSharpLeetCode.Tests
+{
+    [TestFixture]
+    [TestOf(typeof(MissingNumberInArithmeticProgressionSolution))]
+    public class UnitTest1228_MissingNumberInArithmeticProgression
+    {
+        [Test]
+        public void Test_MissingNumberInArithmeticProgressionSolution_FirstCase()
+        {
+            var arr = new[] {5, 7, 11, 13};
+            var solution = new MissingNumberInArithmeticProgressionSolution();
+            
+            var result = solution.MissingNumberLinearSearch(arr);
+            
+            Assert.That(result, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void Test_MissingNumberInArithmeticProgressionSolution_TwentySixthCase()
+        {
+            var arr = new[] { 1, 2, 3, 5 };
+            var solution = new MissingNumberInArithmeticProgressionSolution();
+            
+            var result = solution.MissingNumberLinearSearch(arr);
+            
+            Assert.That(result, Is.EqualTo(4));
+        }
+    }
+}
---
+++ CSharpLeetCode/1228_MissingNumberInArithmeticProgression.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharpLeetCode
+{
+    public class MissingNumberInArithmeticProgressionSolution
+    {
+        public int MissingNumberLinearSearch(int[] arr)
+        {
+            if (arr[^1] == arr[0]) return arr[0];
+            
+            var d = (arr[^1] - arr[0]) / arr.Length;
+            for(var i = 0; i <= arr.Length - 2; i++)
+            {
+                if (arr[i] + d != arr[i + 1])
+                {
+                    return arr[i] + d;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
