cb88f3ceb88285a6ff465d9cc6d0cc181e94f278
kellhusCorp <kellhusCorp@gmail.com>
2024-04-17 12:34:19 +0400

    Added the binary search solution to https://leetcode.com/problems/missing-number-in-arithmetic-progression/

--- CSharpLeetCode.Tests/UnitTest1228_MissingNumberInArithmeticProgression.cs
+++ CSharpLeetCode.Tests/UnitTest1228_MissingNumberInArithmeticProgression.cs
@@ -25,5 +25,27 @@
             
             Assert.That(result, Is.EqualTo(4));
         }
+        
+        [Test]
+        public void Test_MissingNumberInArithmeticProgressionSolution_BinarySearch_FirstCase()
+        {
+            var arr = new[] {5, 7, 11, 13};
+            var solution = new MissingNumberInArithmeticProgressionSolution();
+            
+            var result = solution.MissingNumberBinarySearch(arr);
+            
+            Assert.That(result, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void Test_MissingNumberInArithmeticProgressionSolution_BinarySearch_TwentySixthCase()
+        {
+            var arr = new[] { 1, 2, 3, 5 };
+            var solution = new MissingNumberInArithmeticProgressionSolution();
+            
+            var result = solution.MissingNumberBinarySearch(arr);
+            
+            Assert.That(result, Is.EqualTo(4));
+        }
     }
 }
--- CSharpLeetCode/1228_MissingNumberInArithmeticProgression.cs
+++ CSharpLeetCode/1228_MissingNumberInArithmeticProgression.cs
@@ -17,5 +17,26 @@
 
             return 0;
         }
+
+        public int MissingNumberBinarySearch(int[] arr)
+        {
+            var d = (arr[^1] - arr[0]) / arr.Length;
+            var left = 0;
+            var right = arr.Length - 1;
+            while (left < right)
+            {
+                var middle = left + (right - left) / 2;
+                if (arr[middle] == arr[0] + d * middle)
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle;
+                }
+            }
+
+            return arr[0] + d * left;
+        }
     }
 }
