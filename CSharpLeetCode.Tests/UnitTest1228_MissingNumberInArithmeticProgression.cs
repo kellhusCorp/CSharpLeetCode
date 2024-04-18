612bae6bd67f75607c29fa6d14e9486d482b8d39
Roman <69262893+kellhusCorp@users.noreply.github.com>
2024-04-18 15:20:31 +0400

    Merge pull request #41
    * Added the linear search solution to https://leetcode.com/problems/mis…
    
    * Added the binary search solution to https://leetcode.com/problems/mis…

---
+++ CSharpLeetCode.Tests/UnitTest1228_MissingNumberInArithmeticProgression.cs
@@ -0,0 +1,51 @@
+﻿namespace CSharpLeetCode.Tests
+{
+    [TestFixture]
+    [TestOf(typeof(MissingNumberInArithmeticProgressionSolution))]
+    public class UnitTest1228_MissingNumberInArithmeticProgression
+    {
+        [Test]
+        public void Test_MissingNumberInArithmeticProgressionSolution_FirstCase()
+        {
+            var arr = new[] {5, 7, 11, 13};
+            var solution = new MissingNumberInArithmeticProgressionSolution();
+            
+            var result = solution.MissingNumberLinearSearch(arr);
+            
+            Assert.That(result, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void Test_MissingNumberInArithmeticProgressionSolution_TwentySixthCase()
+        {
+            var arr = new[] { 1, 2, 3, 5 };
+            var solution = new MissingNumberInArithmeticProgressionSolution();
+            
+            var result = solution.MissingNumberLinearSearch(arr);
+            
+            Assert.That(result, Is.EqualTo(4));
+        }
+        
+        [Test]
+        public void Test_MissingNumberInArithmeticProgressionSolution_BinarySearch_FirstCase()
+        {
+            var arr = new[] {5, 7, 11, 13};
+            var solution = new MissingNumberInArithmeticProgressionSolution();
+            
+            var result = solution.MissingNumberBinarySearch(arr);
+            
+            Assert.That(result, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void Test_MissingNumberInArithmeticProgressionSolution_BinarySearch_TwentySixthCase()
+        {
+            var arr = new[] { 1, 2, 3, 5 };
+            var solution = new MissingNumberInArithmeticProgressionSolution();
+            
+            var result = solution.MissingNumberBinarySearch(arr);
+            
+            Assert.That(result, Is.EqualTo(4));
+        }
+    }
+}
---
+++ CSharpLeetCode/1228_MissingNumberInArithmeticProgression.cs
@@ -0,0 +1,42 @@
+﻿namespace CSharpLeetCode
+{
+    public class MissingNumberInArithmeticProgressionSolution
+    {
+        public int MissingNumberLinearSearch(int[] arr)
+        {
+            if (arr[^1] == arr[0]) return arr[0];
+            
+            var d = (arr[^1] - arr[0]) / arr.Length;
+            for(var i = 0; i <= arr.Length - 2; i++)
+            {
+                if (arr[i] + d != arr[i + 1])
+                {
+                    return arr[i] + d;
+                }
+            }
+
+            return 0;
+        }
+
+        public int MissingNumberBinarySearch(int[] arr)
+        {
+            var d = (arr[^1] - arr[0]) / arr.Length;
+            var left = 0;
+            var right = arr.Length - 1;
+            while (left < right)
+            {
+                var middle = left + (right - left) / 2;
+                if (arr[middle] == arr[0] + d * middle)
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle;
+                }
+            }
+
+            return arr[0] + d * left;
+        }
+    }
+}
