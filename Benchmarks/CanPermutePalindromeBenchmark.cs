1f4f8d8b3b3630e4e46fc6cabbb972de47fccfe1
kellhusCorp <kellhusCorp@gmail.com>
2024-04-18 16:12:47 +0400

    Added the solution to https://leetcode.com/problems/palindrome-permutation/

---
+++ Benchmarks/CanPermutePalindromeBenchmark.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using CSharpLeetCode;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    public class CanPermutePalindromeBenchmark
+    {
+        [Params("code", "aab", "carerac", "aa", "aabb")]
+        public string s;
+
+        [Benchmark(Baseline = true)]
+        public bool CanPermutePalindromeClassic()
+        {
+            return new PalindromePermutationSolution().CanPermutePalindrome(s);
+        }
+
+        [Benchmark]
+        public bool CanPermutePalindromeModified()
+        {
+            return new PalindromePermutationSolution().CanPermutePalindromeModified(s);
+        }
+    }
+}
---
+++ Benchmarks/Program.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Benchmarks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var runner = BenchmarkRunner.Run<CanPermutePalindromeBenchmark>();
+    }
+}
