3af5f38bf70bb6bffbe1aad86af27e6a3c751987
Roman <69262893+kellhusCorp@users.noreply.github.com>
2024-04-19 13:39:52 +0400

    Merge pull request #43
    * Added the solution to https://leetcode.com/problems/strictly-palindro…

---
+++ CSharpLeetCode.Tests/UnitTest2396_StrictlyPalindromicNumber.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharpLeetCode.Tests
+{
+    [TestFixture]
+    public class UnitTest2396_StrictlyPalindromicNumber
+    {
+        [Test]
+        [TestCase(9, 3, "100")]
+        [TestCase(50, 2, "110010")]
+        [TestCase(45, 8, "55")]
+        [TestCase(1450, 16, "5AA")]
+        public void Test_StrictlyPalindromicNumberSolution_MoveToBase(int n, int @base, string expectedStr)
+        {
+            var sol = new StrictlyPalindromicNumberSolution();
+
+            var result = sol.MoveToBase(n, @base);
+            
+            Assert.That(result, Is.EqualTo(expectedStr));
+        }
+        
+        [Test]
+        [TestCase(9, false)]
+        [TestCase(4, false)]
+        public void Test_StrictlyPalindromicNumberSolution_IsStrictlyPalindromic(int n, bool expected)
+        {
+            var sol = new StrictlyPalindromicNumberSolution();
+
+            var result = sol.IsStrictlyPalindromic(n);
+            
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ CSharpLeetCode/2396_StrictlyPalindromicNumber.cs
@@ -0,0 +1,59 @@
+﻿namespace CSharpLeetCode
+{
+    public class StrictlyPalindromicNumberSolution
+    {
+        public bool IsStrictlyPalindromic(int n)
+        {
+            var bases = Enumerable.Range(2, n - 2).Select(x => MoveToBase(n, x));
+
+            foreach (string @base in bases)
+            {
+                for (int i = 0, j = @base.Length - 1; i < @base.Length / 2; i++, j--)
+                {
+                    if (@base[i] != @base[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public string MoveToBase(int n, int @base)
+        {
+            Stack<int> stack = new();
+            
+            while (n / @base != 0)
+            {
+                stack.Push(n % @base);
+                n /= @base;
+            }
+            
+            stack.Push(n);
+
+            return @base == 16 ? GetHex(stack) : string.Join(string.Empty, stack);
+        }
+
+        private string GetHex(Stack<int> stack)
+        {
+            var count = stack.Count;
+                
+            char[] array = new char[stack.Count];
+            for (int i = 0; i < count; i++)
+            {
+                int value = stack.Pop();
+                if (value >= 10)
+                {
+                    array[i] = (char)(value + 55);
+                }
+                else
+                {
+                    array[i] = (char)(value + 48);
+                }
+            }
+                
+            return new string(array);
+        }
+    }
+}
