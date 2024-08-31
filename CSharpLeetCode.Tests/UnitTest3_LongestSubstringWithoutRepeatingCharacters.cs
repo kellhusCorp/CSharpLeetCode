b6a030beeeeb892d4c729fbd49bbcefe870e9a01
kellhusCorp <kellhusCorp@gmail.com>
2024-09-01 01:37:14 +0400

    Add the solution to #3

---
+++ CSharpLeetCode.Tests/UnitTest3_LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpLeetCode.Tests
+{
+    public class UnitTest3_LongestSubstringWithoutRepeatingCharacters
+    {
+        [Test]
+        [TestCase("abc", 3)]
+        [TestCase("abcd", 4)]
+        [TestCase("abcabcbb", 3)]
+        [TestCase("bbbbb", 1)]
+        [TestCase("pwwkew", 3)]
+        public void Test_LongestSubstringWithoutRepeatingCharactersSolution_FirstCase(string s, int expected)
+        {
+            var sol = new LongestSubstringWithoutRepeatingCharactersSolution();
+
+            var maxLength = sol.LengthOfLongestSubstring(s);
+            
+            Assert.That(maxLength, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ CSharpLeetCode/3_LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,33 @@
+﻿namespace CSharpLeetCode
+{
+    public class LongestSubstringWithoutRepeatingCharactersSolution
+    {
+        // O(n^2)
+        public int LengthOfLongestSubstring(string s)
+        {
+            var maxLength = 0;
+            for (int startIndex = 0; startIndex < s.Length; startIndex++)
+            {
+                var set = new HashSet<char>
+                {
+                    s[startIndex]
+                };
+                for (int endIndex = startIndex + 1; endIndex < s.Length; endIndex++)
+                {
+                    var result = set.Add(s[endIndex]);
+                    if (!result)
+                    {
+                        break;
+                    }
+                }
+                
+                if (set.Count > maxLength)
+                {
+                    maxLength = set.Count;
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
