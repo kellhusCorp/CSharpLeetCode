137ed9fcafdb8a579125f8848e00d4b247752fa7
Roman <69262893+kellhusCorp@users.noreply.github.com>
2024-10-12 00:48:55 +0400

    Merge pull request #54
    * 1. Add the solution to https://leetcode.com/problems/ternary-expressi…

---
+++ CSharpLeetCode.Tests/UnitTest439_TernaryExpressionParser.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharpLeetCode.Tests
+{
+    public class UnitTest439_TernaryExpressionParser
+    {
+        [Test]
+        [TestCase("T?2:3", "2")]
+        [TestCase("F?1:T?4:5", "4")]
+        [TestCase("T?T?F:5:3", "F")]
+        public void Test_FirstCase(string expression, string expected)
+        {
+            var result = new TernaryExpressionParser().ParseTernary(expression);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ CSharpLeetCode/439_TernaryExpressionParser.cs
@@ -0,0 +1,49 @@
+﻿namespace CSharpLeetCode
+{
+    public class TernaryExpressionParser
+    {
+        public string ParseTernary(string expression)
+        {
+            var stack = new Stack<char>();
+
+            var idx = expression.Length - 1;
+
+            while (idx >= 0)
+            {
+                var c = expression[idx];
+
+                if (IsValue(c))
+                {
+                    stack.Push(c);
+                }
+                else if (c == '?')
+                {
+                    var truthExpression = stack.Pop();
+                    var otherwiseExpression = stack.Pop();
+                    var condition = expression[idx - 1];
+                    stack.Push(SolveExpression(condition, truthExpression, otherwiseExpression));
+                    idx--;
+                }
+                
+                idx--;
+            }
+
+            return stack.Pop().ToString();
+        }
+
+        private static char SolveExpression(char condition, char truthExpression, char otherwiseExpression)
+        {
+            if (condition == 'T')
+            {
+                return truthExpression;
+            }
+
+            return otherwiseExpression;
+        }
+        
+        private static bool IsValue(char c)
+        {
+            return c is >= '0' and <= '9' or 'T' or 'F';
+        }
+    }
+}
